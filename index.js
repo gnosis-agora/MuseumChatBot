a6fe3b286c40526126b59a1344510002f9e18527
gnosis-agora <gnosis-agora@hotmail.com>
2017-07-12 20:51:10 +0800

    Add persistent menu content

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
+// var express = require("express");
+// var request = require("request");
+// var bodyParser = require("body-parser");
+
+import express from "express";
+import request from "request";
+import bodyParser from "body-parser";
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));

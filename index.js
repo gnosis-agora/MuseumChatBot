c5fa2598d335ffdda8afdc177dc50fe38b5a73c1
gnosis-agora <gnosis-agora@hotmail.com>
2017-07-12 22:06:10 +0800

    Add persistent menu content

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function processPostback(event) {
   			sendMessage(senderId, {text: "you've selected history"});
   			break;
   		case "EXPRESSION_START":
-  			sendMessage(senderId, expression_data.START);
+  			sendMessage(senderId, expression_data['START']);
   			break;
   		case "POLITICS_START":
   			sendMessage(senderId, {text: "you've selected politics"});

69239db005d978e977600c445c3bec0b10059f68
gnosis-agora <gnosis-agora@hotmail.com>
2017-07-12 15:39:31 +0800

    Add quick reply buttons

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function processPostback(event) {
         greeting = "Hi " + name + ". ";
       }
       var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
+      sendMessage(senderId, message);
     });
   }
 }
@@ -80,7 +80,21 @@ function sendMessage(recipientId, message) {
     method: "POST",
     json: {
       recipient: {id: recipientId},
-      message: message,
+      message: {
+      	text: message,
+      	quick_replies: [
+      		{
+      			content_type: "text",
+      			title: "Button 1",
+      			payload: "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      		},
+      		{
+      			content_type: "text",
+      			title: "Button 2",
+      			payload: "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_Button2"      			
+      		}
+      	]
+      }
     }
   }, function(error, response, body) {
     if (error) {

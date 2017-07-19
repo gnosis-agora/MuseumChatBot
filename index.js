3ae6fdda00869a51b927297fd0e08ca662aa4112
gnosis-agora <gnosis-agora@hotmail.com>
2017-07-19 23:41:52 +0800

    Use loop / callback approach to send messages in order

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ function processMessage(event) {
         // You may get a text or attachment but not both
         if (message.text) {
         	// deal with all cases here
-        	if (message.hasOwnProperty("quick_reply")) {
+        	if (message.hasOwnProperty("quick_reply") && expression.data[message.quick_reply.payload] !== undefined) {
         		sendMessage(senderId, expression.data[message.quick_reply.payload]);
         	}
           else {

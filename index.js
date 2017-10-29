12cab40295ff6fb98e0708fdc2c71f423fc25f5b
gnosis-agora <gnosis-agora@hotmail.com>
2017-10-29 22:50:02 +0800

    Add typing interface

--- index.js
+++ index.js
@@ -593,8 +593,35 @@ var sendMessage = (recipientId, messages, index=0) => {
       method: "POST",
       json: {
         recipient: {id: recipientId},
-        message: messages[index],
         sender_action: "typing_on"
+      }
+    }, (error, response, body) => {
+      if (error) {
+        console.log("Error sending message: " + response.error);
+      }
+    });
+    setTimeout(() => {
+      request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+          recipient: {id: recipientId},
+          sender_action: "typing_off"
+        }
+      }, (error, response, body) => {
+        if (error) {
+          console.log("Error sending message: " + response.error);
+        }
+      });
+    }, 1000);
+    request({
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: "POST",
+      json: {
+        recipient: {id: recipientId},
+        message: messages[index],
       }
     }, (error, response, body) => {
       if (error) {

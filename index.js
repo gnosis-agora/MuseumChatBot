77fdcf2cd0c021b8e5d6abd1c05e2871b484068c
gnosis-agora <gnosis-agora@hotmail.com>
2017-12-23 00:31:29 +0800

    Test

--- index.js
+++ index.js
@@ -46,35 +46,28 @@ app.get("/webhook", function (req, res) {
   }
 });
 
-// All callbacks for Messenger will be POST-ed here
-// app.post("/webhook", function (req, res) {
-//   // Make sure this is a page subscription
-//   if (req.body.object == "page") {
-//     // Iterate over each entry
-//     // There may be multiple entries if batched
-//     req.body.entry.forEach(function(entry) {
-//       if (entry.messaging) {
-//         // Iterate over each messaging event
-//         entry.messaging.forEach(function(event) {
-//           if (event.postback) {
-//             //processPostback(event);
-//           } 
-//           else if (event.message) {
-//             //processMessage(event);
-//             console.log(event.message);
-//           }
-//         });
-//       }
-//     });
-//     res.status(200).send("EVENT_RECEIVED");
-//   }
-// });
-
+All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function (req, res) {
-  // Make sure this is a page subscription
   console.log(req.body);
-  res.status(200).send("EVENT_RECEIVED");
-  
+  // Make sure this is a page subscription
+  if (req.body.object == "page") {
+    // Iterate over each entry
+    // There may be multiple entries if batched
+    req.body.entry.forEach(function(entry) {
+      if (entry.messaging) {
+        // Iterate over each messaging event
+        entry.messaging.forEach(function(event) {
+          if (event.postback) {
+            processPostback(event);
+          } 
+          else if (event.message) {
+            processMessage(event);
+          }
+        });
+      }
+    });
+    res.status(200).send("EVENT_RECEIVED");
+  }
 });
 
 function processPostback(event) {

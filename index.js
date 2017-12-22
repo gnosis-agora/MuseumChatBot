566ad8c6ac7c526e0733b3159e1bc9dab61f77f7
gnosis-agora <gnosis-agora@hotmail.com>
2017-12-22 23:30:43 +0800

    Test

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ setInterval(() => {
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
+app.listen((process.env.PORT || 5001));
 
 // global variable to keep track of card number
 var cardNumber = 0;
